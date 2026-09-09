4653a6db572c51ade2adba1aa15121c2c2364a9d
Ankit <pandey.ankit275@gmail.com>
2026-09-09 17:31:22 +0530

    Day 2 (Java): Add optional query parameter with default value to /hello endpoint

--- java-api/src/main/java/com/ankit/bigtable_api/GreetingController.java
+++ java-api/src/main/java/com/ankit/bigtable_api/GreetingController.java
@@ -2,14 +2,17 @@ package com.ankit.bigtable_api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
 public class GreetingController {
 
     @GetMapping ("/hello/{name}")
-    public String greet(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String greet(
+        @PathVariable String name,
+        @RequestParam(required = false, defaultValue = "Hello") String greeting) {
+        return greeting + ", " + name + "!";
     }
 
 }
