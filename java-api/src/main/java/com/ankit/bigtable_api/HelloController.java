625779ba7ce70c93fed3aae1b77c01626a7007eb
Ankit <pandey.ankit275@gmail.com>
2026-09-11 02:30:23 +0530

    Day 3 (Java): Add collections/exceptions - Map lookup with custom exception and @ExceptionHandler

--- java-api/src/main/java/com/ankit/bigtable_api/HelloController.java
+++ java-api/src/main/java/com/ankit/bigtable_api/HelloController.java
@@ -1,7 +1,15 @@
 package com.ankit.bigtable_api;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Map;
+import java.util.HashMap;
 
 @RestController
 public class HelloController {
@@ -10,4 +18,30 @@ public class HelloController {
     public String helloWorld() {
         return "Hello World";
     }
+
+    @GetMapping("/age/{name}")
+    public String getAge(@PathVariable String name) {
+    Map<String, Integer> ages = new HashMap<>();
+    ages.put("Ankit", 34);
+    ages.put("Priya", 28);
+
+     if (!ages.containsKey(name)) {
+            throw new PersonNotFoundException("Person not found: " + name);
+        }
+
+    return name + " is " + ages.get(name) + " years old";
+}
+    @ExceptionHandler(PersonNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> handlePersonNotFound(PersonNotFoundException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return error;
+    }
+
+    class PersonNotFoundException extends RuntimeException {
+    public PersonNotFoundException(String message) {
+        super(message);
+    }
+}
 }
